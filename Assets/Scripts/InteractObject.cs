c0828d4e8bb192aee0f0bb75feb1de3391e19349
qiqitaro <thanajan.saes@bumail.net>
2023-12-01 03:44:22 +0700

    add child + fences in guitar scene

--- Assets/Scripts/InteractObject.cs
+++ Assets/Scripts/InteractObject.cs
@@ -69,7 +69,7 @@ public class InteractObject : MonoBehaviour
         targetDistance = Mathf.Sqrt(Mathf.Pow(this.transform.position.x - player.transform.position.x, 2) + 
                                     Mathf.Pow(this.transform.position.z - player.transform.position.z, 2));
         
-        // Debug.Log($"distance from player : {targetDistance}");
+        Debug.Log($"distance from player : {targetDistance}");
     }
 
     private void OnTriggerStay(Collider other)
