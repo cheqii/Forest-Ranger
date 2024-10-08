81bf4f2f47f63735f204fff0fdae64c1381b6128
cheqii <qiqitaro@gmail.com>
2024-10-08 21:24:53 +0700

    fix line renderer

--- Assets/Scripts/LineDirection.cs
+++ Assets/Scripts/LineDirection.cs
@@ -22,11 +22,13 @@ public class LineDirection : MonoBehaviour
             print("show line?");
             LeadPlayerToDestination();
         }
+        
+        line.SetPosition(0, playerPos.transform.position);
     }
 
     public void LeadPlayerToDestination()
     {
-        line.SetPosition(0, playerPos.transform.position);
+        line.gameObject.SetActive(true);
         line.SetPosition(1, destination.transform.position);
     }
 }
