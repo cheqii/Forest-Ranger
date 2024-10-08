2ceea64cfb4004c932a3946177c0d1d110bd75d8
loneguss <pitcha.huta@bumail.net>
2024-10-08 21:33:14 +0700

    line lead to car + new scene

--- Assets/Scripts/LineDirection.cs
+++ Assets/Scripts/LineDirection.cs
@@ -8,6 +8,8 @@ public class LineDirection : MonoBehaviour
     [SerializeField] private Transform destination;
 
     [SerializeField] private LineRenderer line;
+
+    [SerializeField] private Transform carPos;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,11 +24,15 @@ public class LineDirection : MonoBehaviour
             print("show line?");
             LeadPlayerToDestination();
         }
+        
+        line.SetPosition(0, playerPos.transform.position);
     }
 
     public void LeadPlayerToDestination()
     {
-        line.SetPosition(0, playerPos.transform.position);
+        line.gameObject.SetActive(true);
+        //line.SetPosition(0, playerPos.transform.position);
         line.SetPosition(1, destination.transform.position);
+        line.SetPosition(2, carPos.transform.position);
     }
 }
